f8bf439834f195096cd02f1a5a91c51bc6594e26
Colin Brock <colinebrock@gmail.com>
2023-10-20 07:11:56 -0500

    Next step S3 config file upload

--- s3-api/src/s3/s3.controller.ts
+++ s3-api/src/s3/s3.controller.ts
@@ -1,23 +1,37 @@
 // s3.controller.ts
-import { Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { Controller, Get, Post, Req, Res, UseInterceptors } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { S3Service } from './s3.service';
+import { multerConfig } from './multer.config';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('s3')
 export class S3Controller {
   constructor(private readonly s3Service: S3Service) {}
 
   @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
   async uploadFile(@Req() req: Request, @Res() res: Response) {
     try {
-      // Implement the file upload logic here using the S3Service.
-      // You can access the uploaded file in the req object, and use the S3Service to upload it to your S3 bucket.
-      // Respond with a success message or any necessary response.
-    } catch (error) {
-      // Handle any errors that occur during the upload process.
-      // You can respond with an error message or appropriate status code.
+        // Access the uploaded file from the request
+        const uploadedFile = req.file;
+  
+        if (!uploadedFile) {
+          res.status(400).send('No file provided');
+          return;
+        }
+  
+        // Upload the file to S3 using the S3Service
+        await this.s3Service.uploadFile(uploadedFile.originalname, uploadedFile.buffer);
+  
+        // Respond with a success message
+        res.status(200).send('File uploaded successfully');
+      } catch (error) {
+        // Handle errors and respond with an error message
+        console.error('Error uploading file:', error);
+        res.status(500).send('File upload failed');
+      }
     }
-  }
 
   @Get('download')
   async downloadFile(@Req() req: Request, @Res() res: Response) {
